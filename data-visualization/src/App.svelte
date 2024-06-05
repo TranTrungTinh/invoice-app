f6d810d5aaf4849d4195e63093b9678f1534611d
Tinh Tran <tttinh@sk-global.biz>
2024-06-05 09:50:12 +0700

    feat: Add lazy loading for images in data visualization

--- data-visualization/src/App.svelte
+++ data-visualization/src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import AxisX from "$components/AxisX.svelte";
+  import AxisY from "$components/AxisY.svelte";
   import data from "$data/data.js";
 
   import { scaleLinear } from "d3-scale";
@@ -7,24 +9,40 @@
   let width = 400;
   let height = 400;
 
+  const margin = { top: 20, right: 15, bottom: 20, left: 0 };
+  const innerWidth = width - margin.right - margin.left;
+  const innerHeight = height - margin.top - margin.bottom;
+
   let xScale = scaleLinear()
     .domain([0, 100])
-    .range([0, width]);
+    .range([0, innerWidth]);
 
   let yScale = scaleLinear()
     .domain([0, max(data, d => d.hours)])
-    .range([height, 0]);
+    .range([innerHeight, 0]);
 </script>
 
 <svg {width} {height}>
-  {#each data as d}
-    <circle
-      cx={xScale(d.grade)}
-      cy={yScale(d.hours)}
-      r={10}
-      fill="purple"
-      stroke="black"
-      stroke-width={1}
-    />
-  {/each}
+  <g class='inner-chart' transform="translate({margin.left}, {margin.top})">
+    <AxisY width={innerWidth} {yScale} />
+    <AxisX height={innerHeight} width={innerWidth} {xScale} />
+    {#each data as d}
+      <circle
+        cx={xScale(d.grade)}
+        cy={yScale(d.hours)}
+        r={10}
+        fill="purple"
+        stroke="black"
+        stroke-width={1}
+      />
+    {/each}
+  </g>
 </svg>
+
+<style>
+  :global(.tick text, .axis-title) {
+    font-weight: 400; /* How thick our text is */
+    font-size: 12px; /* How big our text is */
+    fill: hsla(212, 10%, 53%, 1); /* The color of our text */
+  }
+</style>
--- data-visualization/vite.config.js
+++ data-visualization/vite.config.js
@@ -5,6 +5,13 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  vite: {
+    server: {
+      hmr: {
+        overlay: false
+      }
+    }
+  },
   resolve: {
     alias: {
       $components: path.resolve("./src/components"),
