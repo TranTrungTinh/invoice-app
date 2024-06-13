ecc65c9c831dd1e50b045a8c31db86c7c44b4da5
Tinh Tran <tttinh@sk-global.biz>
2024-06-13 09:22:04 +0700

    feat: Add color scale and radius scale to data visualization

--- data-visualization/src/App.svelte
+++ data-visualization/src/App.svelte
@@ -1,15 +1,14 @@
 <script>
   import data from "$data/data.js";
   import { forceSimulation, forceX, forceY, forceCollide } from "d3-force";
-  import { scaleLinear, scaleBand } from "d3-scale";
+  import { scaleLinear, scaleBand, scaleOrdinal, scaleSqrt } from "d3-scale";
 
   let width = 400,
     height = 400;
   const margin = { top: 0, right: 0, left: 0, bottom: 20 };
-  const RADIUS = 5;
 
-  $: innerWidth = width - margin.right - margin.left;
-  let innerHeight = height - margin.top - margin.bottom;
+  $: innerWidth = width - margin.left - margin.right;
+  let innerHeight = height - margin.bottom - margin.top;
 
   import { mean, rollups } from "d3-array";
 
@@ -22,6 +21,23 @@
     .sort((a, b) => a[1] - b[1]) // Sort according to value
     .map(d => d[0]); // Grab the continent name
 
+  const colorRange = [
+    "#dda0dd",
+    "#fe7f2d",
+    "#fcca46",
+    "#a1c181",
+    "#619b8a",
+    "#eae2b7"
+  ];
+
+  let colorScale = scaleOrdinal()
+    .domain(continents) // continents was already defined in our code
+    .range(colorRange);
+
+  $: radiusScale = scaleSqrt()
+    .domain([1, 9]) // The same domain passed to xScale
+    .range(width < 568 ? [2, 6] : [3, 8]);
+
   $: xScale = scaleLinear()
     .domain([1, 9]) // Alternatively, we could pass .domain(extent(data, d => d.happiness))
     .range([0, innerWidth]);
@@ -31,7 +47,11 @@
     .range([innerHeight, 0])
     .paddingOuter(0.5);
 
-  const simulation = forceSimulation(data);
+  let simulation = forceSimulation(data);
+  let nodes = [];
+  simulation.on("tick", () => {
+    nodes = simulation.nodes();
+  });
 
   $: {
     simulation
@@ -47,26 +67,20 @@
           .y(d => yScale(d.continent))
           .strength(0.2)
       )
-      .force("collide", forceCollide().radius(RADIUS))
+      .force("collide", forceCollide().radius(d => radiusScale(d.happiness)))
       .alpha(0.3) // [0, 1] The rate at which the simulation finishes. You should increase this if you want a faster simulation, or decrease it if you want more "movement" in the simulation.
       .alphaDecay(0.0005) // [0, 1] The rate at which the simulation alpha approaches 0. you should decrease this if your bubbles are not completing their transitions between simulation states.
-      // .stop()
-      .restart()
+      .restart();
   }
 
-  let nodes = [];
-  simulation.on("tick", () => {
-    nodes = simulation.nodes();
-  });
-
-  $: console.log(nodes);
+  $: console.log(simulation);
 </script>
 
 <div class='chart-container' bind:clientWidth={width}>
   <svg {width} {height}>
     <g class="inner-chart" transform="translate({margin.left}, {margin.top})">
       {#each nodes as node}
-        <circle cx={node.x} cy={node.y} r={RADIUS} fill="steelblue" stroke="black" />
+        <circle cx={node.x} cy={node.y} r={radiusScale(node.happiness)} fill={colorScale(node.continent)} stroke="black" />
       {/each}
     </g>
   </svg>
