cbe1fd44666b453130da1cf81936fec516c2f566
Tinh Tran <tttinh@sk-global.biz>
2024-07-09 10:54:50 +0700

    refactor: Improve chart layout and add Tooltip component in data visualization

--- data-visualization/src/App.svelte
+++ data-visualization/src/App.svelte
@@ -10,17 +10,17 @@
   let width = 400,
     height = 400;
 
-  const margin = { top: 20, right: 15, bottom: 20, left: 0 };
+  const margin = { top: 25, right: 25, bottom: 30, left: 10 };
   const radius = 10;
 
   $: innerWidth = width - margin.left - margin.right;
-  let innerHeight = height - margin.top - margin.bottom;
+  $: innerHeight = height - margin.top - margin.bottom;
 
   $: xScale = scaleLinear()
     .domain([0, 100])
     .range([0, innerWidth]);
 
-  let yScale = scaleLinear()
+  $: yScale = scaleLinear()
     .domain([0, max(data, d => d.hours)])
     .range([innerHeight, 0]);
 
@@ -54,14 +54,19 @@
       hoveredData = initialData[13];
     }
   }
+
+  import SampleText from "$components/SampleText.svelte";
 </script>
 
+<main>
+<h1>Students who studied longer scored higher on their final exams</h1>
+<SampleText />
 <section>
 <div class='sticky'>
-  <h1>Students who studied longer scored higher on their final exams</h1>
   <div
     class="chart-container"
     bind:clientWidth={width}
+    bind:clientHeight={height}
   >
     <svg {width} {height} on:mouseleave={() => hoveredData = null}>
       <g class="inner-chart" transform="translate({margin.left}, {margin.top})">
@@ -79,7 +84,7 @@
               fill="purple"
               stroke="black"
               r={hoveredData == d ? radius * 2 : radius}
-              opacity={hoveredData ? (hoveredData == d ? 1 : 0.45) : 0.85}
+              opacity={hoveredData ? (hoveredData == d ? 1 : 0.45) : 0.85}              
               on:mouseover={() => (currentStep >= 2 ? hoveredData = d : null)}
               on:focus={() => (currentStep >= 2 ? hoveredData = d : null)}
               tabindex="0"
@@ -88,7 +93,7 @@
       </g>
     </svg>
     {#if hoveredData}
-        <Tooltip {xScale} {yScale} {width} data={hoveredData} />
+      <Tooltip {xScale} {yScale} {width} data={hoveredData} />
     {/if}
   </div>
 </div>
@@ -105,8 +110,16 @@
     </Scrolly>
   </div>
 </section>
+<SampleText />
+</main>
 
 <style>
+  main {
+    max-width: 768px;
+    margin: auto;
+    padding: 8px;
+  }
+
   :global(.tick text, .axis-title) {
     font-weight: 400;
     font-size: 12px;
@@ -125,7 +138,7 @@
   }
 
   h1 {
-    margin: 0 0 0.5rem 0;
+    margin: 0.5rem 0 1rem 0;
     font-size: 1.35rem;
     font-weight: 600;
   }
@@ -149,8 +162,34 @@
 
   .sticky {
     position: sticky;
+
     top: 0;
     z-index: 1;
+
+    height: 95vh;
+    width: 100%;
+    top: 2.5vh;
+    margin-bottom: 1rem;
+
+    /* Optional */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .chart-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    /* Optional */
+    max-width: 700px;
+    max-height: 450px;
+
+    background: white;
+    box-shadow: 1px 1px 30px rgba(252, 220, 252, 1);
+    border: 1px solid plum;
+    border-radius: 6px;
   }
 
   .steps {
